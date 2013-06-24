633ccd697382f27913f2dcbedeeb0b8f6aa4d3b2
april <april@ecbank-fe3119d4>
2013-06-24 16:38:21 +0900

    wget(): download web file

--- untitled/first.py
+++ untitled/first.py
@@ -1,5 +1,38 @@
 import os
+import sys
+from bs4 import BeautifulSoup
+import urllib.request
 
-if __name__ == 'hello':
-    print('hello')
+def wget(savefile = '', url = '', url_encoding = 'utf8'):
+    user_agent = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'
+    
+    if url == '' or savefile == '':
+        return
+    
+    try:
+        if url[0:4] == 'http':
+            url = url.replace("http://", '')
+            
+    except NameError as e:
+        sys.stderr.write(e)
+        return
+        
+    url = urllib.request.quote(url, safe = '\/|\&|\?|\=', encoding = url_encoding)
+    url = 'http://' + url
+    
+    try:
+        req = urllib.request.Request(url)
+        req.add_header('user-agent', user_agent)
+        req.add_header('referer', url)
+        
+        data = urllib.request.urlopen(req).read()
+        with open(savefile, 'wb') as f:
+            f.write(data)
+        
+    except Exception as e:
+        sys.stderr.error(e)
+        
+    
+if __name__ == '__main__':
+    wget("d:/tmp/aaa.txt", "http://www.khgames.co.kr/news/photo/201306/64528_6485_5350.jpg" )
     
