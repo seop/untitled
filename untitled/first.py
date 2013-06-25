21ecec96ad19318bceea3ab6cd3bc2073f359bee
seop <ara@live.com>
2013-06-25 18:22:56 +0900

    add get_page()

--- untitled/first.py
+++ untitled/first.py
@@ -32,6 +32,23 @@ def wget(savefile = '', url = '', url_encoding = 'utf8'):
     except Exception as e:
         sys.stderr.error(e)
         
+def get_page(url, encoding = 'utf8'):
+    user_agent = 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'
+    
+    if url == '':
+        return
+    
+    try:
+        if url[0:4] == 'http':
+            url = url.replace("http://", '')
+            
+    except NameError as e:
+        sys.stderr.write(e)
+        return
+        
+    url = urllib.request.quote(url, safe = '\/|\&|\?|\=', encoding = encoding)
+    url = 'http://' + url
+    
     
 if __name__ == '__main__':
     wget("d:/tmp/aaa.txt", "" )
